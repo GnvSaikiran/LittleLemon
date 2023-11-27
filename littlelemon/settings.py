bf9b3c15a43319249819326d449e1da085b28e3b
Gnv Saikiran <61322778+GnvSaikiran@users.noreply.github.com>
2023-11-27 13:35:43 +0530

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'saikiran',
+        'PASSWORD': 'root',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
