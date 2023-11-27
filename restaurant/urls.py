e52e8159b2818fc1988dd70b92c52591dbc8e6a2
gnv <gajjarapusai1@gmail.com>
2023-11-27 12:19:37 +0530

    Add Menu API and Booking API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu', views.MenuItemView.as_view(), name='menu_items'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu_item'),
 ]
